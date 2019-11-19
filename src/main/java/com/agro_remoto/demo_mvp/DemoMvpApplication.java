1c0b892783cd1b27837091e4a0de0e348a514904
Joana Campos <cgsrjunior@inf.ufrgs.br>
2019-11-19 10:00:29 -0300

    Intial backbone from project

---
+++ src/main/java/com/agro_remoto/demo_mvp/DemoMvpApplication.java
@@ -0,0 +1,13 @@
+package com.agro_remoto.demo_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agro_remoto/demo_mvp/DemoMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agro_remoto.demo_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
